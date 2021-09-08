f74ab822342e59f73ea257b3db8c692d43603716
vhrvoje2 <vhrvoje2@yahoo.com>
2021-09-08 18:33:47 +0200

    Full-Featured Web App Part 3 - Forms and User Input

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "38699dbd0c9b86f7b482de80805a7fc9"
 
 
 posts = [
@@ -7,15 +11,16 @@ posts = [
         "author": "Corey Schafer",
         "title": "Blog Post 1",
         "content": "First post content",
-        "date_posted": "April 20, 2018"
+        "date_posted": "April 20, 2018",
     },
     {
         "author": "Jane Doe",
         "title": "Blog Post 2",
         "content": "Second post content",
-        "date_posted": "April 21, 2018"
+        "date_posted": "April 21, 2018",
     },
 ]
+
 
 @app.route("/")
 @app.route("/home")
@@ -26,6 +31,31 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", category="success")
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", category="success")
+            return redirect(url_for("home"))
+        else:
+            flash(
+                "Login unsuccessful. Please check username and password.",
+                category="danger",
+            )
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
