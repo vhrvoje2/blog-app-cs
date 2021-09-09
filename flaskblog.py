c5272ebe0250ae19e2dea17b66f720c501917243
vhrvoje2 <vhrvoje2@yahoo.com>
2021-09-09 17:57:41 +0200

    Full-Featured Web App Part 4 - Database with Flask-SQLAlchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,43 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from os import path
 
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = "38699dbd0c9b86f7b482de80805a7fc9"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+
+
+def create_database(app):
+    if not path.exists(f"site.db"):
+        db.create_all(app=app)
+        print("\t* Database created!")
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    avatar_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.avatar_file}'))"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 posts = [
@@ -59,4 +93,6 @@ def login():
 
 
 if __name__ == "__main__":
+    db.init_app(app)
+    create_database(app)
     app.run(debug=True)
